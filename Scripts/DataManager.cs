28ccc8d119ebdac8ee83bb52ecbd86bdc240dfb2
정창영 <54877137+bfcat46@users.noreply.github.com>
2024-05-23 09:50:17 +0900

    scriptsfile

---
+++ Scripts/DataManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager instance;
+
+    public int characterNum;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
